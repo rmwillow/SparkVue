25208413b595ed5b489884b8330eb54a1e50aff0
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-08-24 19:23:38 -0500

    create user

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -30,7 +30,12 @@ const userController = {
             });
     },
 
-
+    // createUser
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    },
 }
 
 module.exports = userController;
