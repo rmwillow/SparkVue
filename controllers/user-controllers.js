c682d984ac68c982e6fa3c417d34158fe19d92ac
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-08-24 19:22:49 -0500

    get one user id

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -2,19 +2,35 @@ const { User } = require('../models');
 
 const userController = {
     getAllUser(req, res) {
-      User.find({})
-        .populate({
-          path: 'reactions',
-          select: '-__v'
-        })
-        .select('-__v')
-        .sort({ _id: -1 })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-          console.log(err);
-          res.sendStatus(400);
-        });
-      },
-    }
+        User.find({})
+            .populate({
+                path: 'reactions',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+    // get one user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            .populate({
+                path: 'reactions',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+
+
+}
 
 module.exports = userController;
