5d7067542219ad4f00a6cc114dab0b5d909e1cbf
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-08-24 19:24:46 -0500

    update user

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -36,6 +36,20 @@ const userController = {
             .then(dbUserData => res.json(dbUserData))
             .catch(err => res.status(400).json(err));
     },
+    // update user by id
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+
+
 }
 
 module.exports = userController;
