e60e180ad2b97d9b18b23afb1b448978fdf2dd51
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-08-25 12:57:40 -0500

    index.js routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,8 +1,5 @@
 const router = require('express').Router();
 
-router.use('/thoughts', thoughtRoutes);
 
-// add prefix of `/users` to routes created in `user-routes.js`
-router.use('/users', userRoutes);
 
 module.exports = router;
