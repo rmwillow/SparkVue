c39101d83618490b6d525d271b97459ca5269c72
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-08-24 19:43:35 -0500

    remove reaction

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -78,8 +78,37 @@ const thoughtController = {
             .catch(err => res.json(err));
     },
 
+    // remove reaction
+    removeReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reactions: { reactionId: params.reactionId } } },
+            { new: true }
+        )
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err));
+    },
+    removeThought({ params }, res) {
+        Thought.findOneAndDelete({ _id: params.commentId })
+            .then(deletedThought => {
+                if (!deletedThought) {
+                    return res.status(404).json({ message: 'No thought with this id!' });
+                }
+                return User.findOneAndUpdate(
+                    { _id: params.userId },
+                    { $pull: { thoughts: params.thoughtId } },
+                    { new: true }
+                );
+            })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
 }
-
-
 
 module.exports = thoughtController;
