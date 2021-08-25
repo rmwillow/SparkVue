071e29546429a6a20fc4abe09155b1b50ee0e9d0
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-08-24 19:26:18 -0500

    delete user and friend, create friend

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -49,7 +49,38 @@ const userController = {
             .catch(err => res.json(err));
     },
 
+    // delete user
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
 
+    // createFriend
+    createFriend({ body }, res) {
+        Friend.create(body)
+            .then(dbFriendData => res.json(dbFriendData))
+            .catch(err => res.status(400).json(err));
+    },
+    // delete Friend
+    deleteFriend({ params }, res) {
+        Friend.findOneAndDelete({ _id: params.id })
+            .then(dbFriendData => {
+                if (!dbFriendData) {
+                    res.status(404).json({ message: 'No friend found with this id!' });
+                    return;
+                }
+                res.json(dbFriendData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
 }
+
 
 module.exports = userController;
