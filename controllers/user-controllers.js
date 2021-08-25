bc26f7a368eb60aec12dfa2fdcbc77fc62c5fe23
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-08-24 19:21:37 -0500

    added user controller

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,3 +1,20 @@
 const { User } = require('../models');
 
+const userController = {
+    getAllUser(req, res) {
+      User.find({})
+        .populate({
+          path: 'reactions',
+          select: '-__v'
+        })
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+          console.log(err);
+          res.sendStatus(400);
+        });
+      },
+    }
+
 module.exports = userController;
