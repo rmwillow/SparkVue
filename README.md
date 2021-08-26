29f0d2af32f49a2ffbdb00b4da0844404358d55c
Rachel McGrath <52016382+rmwillow@users.noreply.github.com>
2021-08-26 10:08:50 -0500

    Develop (#1)
    * created files/folders
    
    * added packages and user file
    
    * setting up express and mongoose
    
    * user schema
    
    * friend count
    
    * reply schema
    
    * thought schema
    
    * get friends count
    
    * exporting models
    
    * created user controller & export
    
    * added user controller
    
    * get one user id
    
    * create user
    
    * update user
    
    * delete user and friend, create friend
    
    * created thought controller
    
    * get one user id
    
    * update thought by id
    
    * remove reaction
    
    * export user & thought models
    
    * thought and user routes created
    
    * index.js routes
    
    * thought routes
    
    * user routes added & reformated
    
    * updated controllers
    
    * unique username and email to user model
    
    * added reaction model
    
    * mongoose error and controllers bug ficxed
    
    * all routes function and insomnia testing completed
    
    * fixed reaction controllers
    
    * completed read me

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # SparkVue
+
+An API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list. This application uses Express, Node, MongoDB and Mongoose to allow users, thoughts, friend lists and thought reactions to be created, updated and deleted. 
+
+## User Story
+As a social media startup, I want an API for my social network that uses a NoSQL database so that my website can handle large amounts of unstructured data
+
+## Walkthrough Video
+https://drive.google.com/file/d/1kzhH45QgbuBWNgL6rpGHQ0hCZipLZJI2/view
+
+## Models
+- User
+- Thought
+- Reaction (used as a subdocument in Thought)
+
+## Endpoints
+**User**
+- Get all users:        `GET /api/users`
+- Create a user:        `POST /api/users`
+- Get user by ID:       `GET /api/users/:id`
+- Update a user:        `PUT /api/users/:id`
+- Delete a user:        `DELETE /api/users/:id`
+- Add a friend:         `PUT /api/users/:userId/friends/:friendId`
+- Delete a friend:      `DELETE /api/users/:userId/friends/:friendId`
+
+**Thought**
+- Get all thoughts:     `GET /api/thoughts`
+- Create a thought:     `POST /api/thoughts`
+- Get thought by ID:    `GET /api/thoughts/:id`
+- Update a thought:     `PUT /api/thoughts/:id`
+- Delete a thought:     `DELETE /api/thoughts/:id`
+
+**Reaction**
+- Add a reaction:       `PUT /api/thoughts/:id/reactions`
+- Delete a reaction:    `DELETE /api/thoughts/:id/reactions`
+
+
+## Technologies 
+* JavaScript
+* Node.js
+* MongoDB
+* Mongoose
+* Express.js
+
+## Installation 
+* Clone GitHub repository
+* npm init
+* Ensure that MongoDB and Express are installed on your computer. 
+* Invoke application using npm start. 
+
+## License
+MIT license
---
+++ controllers/thought-controller.js
@@ -0,0 +1,152 @@
+const { User, Thought, Reaction } = require('../models');
+
+const thoughtController = {
+
+    // GET /api/thoughts
+    getAllThoughts(req, res) {
+        Thought.find({})
+        .populate({
+            path: 'user',
+            select: '-__v'
+        })
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err)
+        });
+    },
+
+    // GET /api/thoughts/:id
+    //get one thought by ID
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+            .populate({
+                path: 'user',
+                select: '-__v'
+            })
+           .select('-__v')
+           .sort({ _id: -1 })
+           .then(dbThoughtData => res.json(dbThoughtData))
+           .catch(err => {
+               console.log(err);
+               res.status(500).json(err)
+           })
+    },
+
+
+    // POST /api/thoughts
+    // expected body:
+    // {
+    //     "thoughtText": "foo",
+    //     "username": "bar",  // should be a username that corresponds to a User instance
+    //     "userId": "baz"  // should be a userId that corresponds to the same User instance as username
+    // }
+    //create thought
+    createThought({ params, body }, res) {
+        Thought.create(body)
+            .then(({ _id}) => {
+                return User.findOneAndUpdate(
+                    { username: body.username },
+                    { $push: { thoughts: _id } },
+                    { new: true }
+                );
+            })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this username!'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+
+
+    // PUT /api/thoughts/:id
+    // expected body should include at least one of the following attributes:
+    // {
+    //     "thoughtText": "foo",
+    //     "username": "bar",  // should be a username that corresponds to a User instance
+    //     "userId": "baz"  // should be a userId that corresponds to the same User instance as username
+    // }
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.id },
+            body,
+            { new: true }
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this id' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+
+    // DELETE /api/thoughts/:id
+    deleteThought({ params }, res) {
+        // delete the thought
+        Thought.findOneAndDelete({ _id: params.id })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this id'});
+                return;
+            }
+            // delete the reference to deleted thought in user's thought array
+            User.findOneAndUpdate(
+                { username: dbThoughtData.username },
+                { $pull: { thoughts: params.id } }
+            )
+            .then(() => {
+                res.json({message: 'Successfully deleted the thought'});
+            })
+            .catch(err => res.status(500).json(err));
+        })
+        .catch(err => res.status(500).json(err));
+    },
+
+    // POST /api/thoughts/:id/reactions
+    addReaction ({ params, body}, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $push: { reactions: body } },
+            { new: true, runValidators: true }
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought with this ID!' });
+                return;
+            }
+            res.json(dbThoughtData)
+        })
+        .catch(err => res.json(err));
+    },
+
+    // DELETE /api/thoughts/:id/reactions
+    // expected body should include at least one of the following attributes:
+    // {
+    //     "reactionId": "baz"  // should be a reactionId in the specified Thought instance
+    // }
+    deleteReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reactions: { reactionId: body.reactionId } } },
+            { new: true, runValidators: true }
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this id' });
+                return;
+            }
+            res.json({message: 'Successfully deleted the reaction'});
+        })
+        .catch(err => res.status(500).json(err));
+    },
+}
+
+module.exports = thoughtController;
